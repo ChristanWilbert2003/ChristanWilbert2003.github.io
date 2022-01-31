dfee2ccaac46281a4ec76f26b2edfa17a94fd9e8
Christan Wilbert <cwchristan@gmail.com>
2022-01-31 11:45:26 +0530

    deployed version 1.0

--- server.js
+++ server.js
@@ -8,3 +8,19 @@ server.use(middlewares);
 server.use(router);
 
 server.listen(port);
+
+
+
+const jsonServer =require('json-server');
+const server = jsonServer.create();
+const router =jsonServer.router('./data/db.json');
+const middlewares = jsonServer.defaults();
+const PORT = process.env.PORT || 8080;
+server.use(middlewares);
+server.use(jsonServer.rewriter({
+    '/public/images*':'/$1',
+}))
+server.use(router);
+server.listen(PORT,() => {
+    console.log("Server is running");
+});
