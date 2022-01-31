7ed16f53f13504c78bcc82ba2645f33f4455f989
Christan Wilbert <cwchristan@gmail.com>
2022-01-31 16:31:48 +0530

    Deployment 1.5 version 1.1

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 const jsonServer =require('json-server');
 const server = jsonServer.create();
-const router =jsonServer.router('./data');
+const router =jsonServer.router('./data/db.json');
 const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 8080;
 server.use(middlewares);
